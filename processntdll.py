44860717163a755d8516582c00f56504c3281fdc
nodes <159713604+LNodesL@users.noreply.github.com>
2024-03-30 09:24:52 -0400

    Update processntdll.py

--- processntdll.py
+++ processntdll.py
@@ -1,3 +1,5 @@
+# Python example script that parses & converts to the markdown style table found in ntdll.md - from the raw ntdll.dll data that is in ntdll.txt! (The ntdll.txt data is from an IDA Freeware export - select all in left functions column, copy, and paste into Notepad) 
+
 def format_to_markdown_table(file_path):
     with open(file_path, 'r') as file:
         # Read the file content
